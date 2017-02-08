b66ae6ccd9b2c456984f45f9e541a0d81a141962
Matthew Hiler <mahiler@columbus.rr.com>
2017-02-08 18:50:44 -0500

    Added views

--- Controllers/AutoController.cs
+++ Controllers/AutoController.cs
@@ -4,11 +4,13 @@ public class AutoController : Controller
 {
     public IActionResult Index()
     {
-        return Ok("This is the index of the AutoController");
+        //return Ok("This is the index of the AutoController");
+        return View();
     }
 
     public IActionResult Quote()
     {
-        return Ok("This is your auto insurance quote");
+        //return Ok("This is your auto insurance quote");
+        return View();
     }
 }
--- Controllers/HomeInsuranceController.cs
+++ Controllers/HomeInsuranceController.cs
@@ -4,11 +4,13 @@ public class HomeInsuranceController : Controller
 {
     public IActionResult Index()
     {
-        return Ok("This is the index of the HomeInsuranceController");
+        //return Ok("This is the index of the HomeInsuranceController");
+        return View();
     }
 
     public IActionResult Quote()
     {
-        return Ok("This is your home insurance quote");
+       //return Ok("This is your home insurance quote");
+       return View();
     }
 }
