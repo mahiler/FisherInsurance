c52c5591fdc1a26c439547f3dd04c81803e2abf4
Matthew Hiler <mahiler@columbus.rr.com>
2017-02-13 18:52:58 -0500

    new additions

--- Controllers/CustomerCareController.cs
+++ Controllers/CustomerCareController.cs
@@ -1,24 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 
 [Route("customer")]
-
 public class CustomerCareController : Controller
 {
     public IActionResult Index()
     {
-        return Ok("This is the index of the CustomerCareController");
+        //return Ok("This is the index of the CustomerCareController");
+        return View();
     }
 
      [Route("myclaims")]
      public IActionResult ClaimHistory()
     {
-        return Ok("This is your customer care claims");
+        //return Ok("This is your customer care claims");
+        return View();
 
     }
     
     [Route("fileclaim")]
     public IActionResult NewClaim()
     {
-        return Ok("This is where you file a new claim");
+        //return Ok("This is where you file a new claim");
+        return View();
     }
 }
